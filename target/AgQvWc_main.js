3b83c350624e5c722a5ab5931c7f5f0822a2cbe7
YourBoiiLevi <227216007+YourBoiiLevi@users.noreply.github.com>
2025-11-05 10:01:33 -0500

    Add final target file AgQvWc_main.js

---
+++ target/AgQvWc_main.js
@@ -0,0 +1,394 @@
+Error compiling input:
+
+Line 49: Parse error. syntax error
+Line 75: Parse error. missing ; before statement
+Line 76: Parse error. missing ; before statement
+Line 77: Parse error. syntax error
+Line 88: Parse error. syntax error
+Line 89: Parse error. syntax error
+Line 95: Parse error. 'try' without 'catch' or 'finally'
+Line 97: Parse error. missing ) in parenthetical
+Line 208: Parse error. syntax error
+Line 209: Parse error. syntax error
+Line 225: Parse error. missing ; before statement
+Line 226: Parse error. missing ; before statement
+Line 228: Parse error. syntax error
+Line 229: Parse error. missing ; before statement
+Line 230: Parse error. missing ; before statement
+Line 231: Parse error. syntax error
+Line 232: Parse error. syntax error
+Line 233: Parse error. syntax error
+Line 242: Parse error. syntax error
+Line 243: Parse error. missing ; before statement
+Line 244: Parse error. syntax error
+Line 249: Parse error. syntax error
+Line 250: Parse error. missing ; before statement
+Line 251: Parse error. missing ; before statement
+Line 253: Parse error. syntax error
+Line 254: Parse error. missing ; before statement
+Line 255: Parse error. syntax error
+Line 256: Parse error. syntax error
+Line 258: Parse error. syntax error
+Line 259: Parse error. missing ; before statement
+Line 260: Parse error. syntax error
+Line 261: Parse error. syntax error
+Line 262: Parse error. missing ; before statement
+Line 264: Parse error. syntax error
+Line 265: Parse error. missing ; before statement
+Line 266: Parse error. syntax error
+Line 268: Parse error. syntax error
+Line 269: Parse error. missing ; before statement
+Line 271: Parse error. syntax error
+Line 272: Parse error. missing ; before statement
+Line 273: Parse error. syntax error
+Line 275: Parse error. syntax error
+Line 276: Parse error. missing ; before statement
+Line 278: Parse error. syntax error
+Line 279: Parse error. missing ; before statement
+Line 280: Parse error. syntax error
+Line 282: Parse error. syntax error
+Line 283: Parse error. missing ; before statement
+Line 285: Parse error. syntax error
+Line 286: Parse error. missing ; before statement
+Line 287: Parse error. syntax error
+Line 292: Parse error. syntax error
+Line 293: Parse error. missing ; before statement
+Line 294: Parse error. missing ; before statement
+Line 295: Parse error. missing ; before statement
+Line 297: Parse error. syntax error
+Line 299: Parse error. syntax error
+Line 300: Parse error. syntax error
+Line 302: Parse error. syntax error
+Line 304: Parse error. missing ; before statement
+Line 305: Parse error. missing ; before statement
+Line 306: Parse error. missing ; before statement
+Line 308: Parse error. syntax error
+Line 310: Parse error. syntax error
+Line 311: Parse error. syntax error
+Line 313: Parse error. syntax error
+Line 315: Parse error. syntax error
+Line 317: Parse error. missing ; before statement
+Line 318: Parse error. syntax error
+Line 319: Parse error. syntax error
+Line 320: Parse error. syntax error
+Line 321: Parse error. syntax error
+Line 336: Parse error. syntax error
+Line 339: Parse error. syntax error
+Line 340: Parse error. syntax error
+Line 341: Parse error. syntax error
+Line 342: Parse error. syntax error
+Line 343: Parse error. syntax error
+Line 344: Parse error. syntax error
+Line 349: Parse error. syntax error
+Line 350: Parse error. syntax error
+Line 351: Parse error. syntax error
+Line 360: Parse error. syntax error
+Line 361: Parse error. syntax error
+Line 363: Parse error. syntax error
+Line 364: Parse error. syntax error
+Line 365: Parse error. syntax error
+Line 367: Parse error. syntax error
+Line 368: Parse error. syntax error
+Line 369: Parse error. syntax error
+Line 372: Parse error. syntax error
+Line 373: Parse error. syntax error
+Line 374: Parse error. syntax error
+Line 380: Parse error. syntax error
+Line 381: Parse error. syntax error
+Line 392: Parse error. syntax error
+Line 393: Parse error. syntax error
+Line 394: Parse error. missing ; before statement
+Line 395: Parse error. syntax error
+Line 397: Parse error. syntax error
+Line 398: Parse error. syntax error
+Line 399: Parse error. syntax error
+Line 400: Parse error. missing ; before statement
+Line 401: Parse error. missing ; before statement
+Line 402: Parse error. missing ; before statement
+Line 403: Parse error. syntax error
+Line 405: Parse error. syntax error
+Line 406: Parse error. syntax error
+Line 422: Parse error. syntax error
+Line 424: Parse error. syntax error
+Line 425: Parse error. missing ; before statement
+Line 426: Parse error. syntax error
+Line 427: Parse error. syntax error
+Line 428: Parse error. syntax error
+Line 430: Parse error. syntax error
+Line 431: Parse error. syntax error
+Line 432: Parse error. missing ; before statement
+Line 454: Parse error. syntax error
+Line 455: Parse error. missing ; before statement
+Line 456: Parse error. missing ; before statement
+Line 457: Parse error. syntax error
+Line 458: Parse error. syntax error
+Line 460: Parse error. missing ; before statement
+Line 461: Parse error. missing ; before statement
+Line 462: Parse error. syntax error
+Line 471: Parse error. syntax error
+Line 472: Parse error. syntax error
+Line 474: Parse error. missing ; before statement
+Line 476: Parse error. syntax error
+Line 477: Parse error. syntax error
+Line 478: Parse error. missing ; before statement
+Line 480: Parse error. syntax error
+Line 482: Parse error. syntax error
+Line 484: Parse error. missing ; before statement
+Line 486: Parse error. syntax error
+Line 487: Parse error. syntax error
+Line 488: Parse error. syntax error
+Line 489: Parse error. syntax error
+Line 489: Parse error. illegal character
+Line 489: Parse error. missing ; before statement
+Line 489: Parse error. illegal character
+Line 490: Parse error. illegal character
+Line 490: Parse error. missing ; before statement
+Line 490: Parse error. illegal character
+Line 491: Parse error. illegal character
+Line 491: Parse error. missing ; before statement
+Line 491: Parse error. illegal character
+Line 492: Parse error. syntax error
+Line 493: Parse error. missing ; before statement
+Line 494: Parse error. syntax error
+Line 496: Parse error. syntax error
+Line 497: Parse error. missing ; before statement
+Line 498: Parse error. syntax error
+Line 500: Parse error. syntax error
+Line 501: Parse error. missing ; before statement
+Line 502: Parse error. missing ; before statement
+Line 503: Parse error. syntax error
+Line 504: Parse error. syntax error
+Line 506: Parse error. missing ; before statement
+Line 507: Parse error. syntax error
+Line 509: Parse error. syntax error
+Line 510: Parse error. missing ; before statement
+Line 511: Parse error. syntax error
+Line 512: Parse error. syntax error
+Line 513: Parse error. missing ; before statement
+Line 514: Parse error. syntax error
+Line 515: Parse error. syntax error
+Line 516: Parse error. syntax error
+Line 517: Parse error. syntax error
+Line 518: Parse error. syntax error
+Line 519: Parse error. syntax error
+Line 520: Parse error. syntax error
+Line 521: Parse error. syntax error
+Line 563: Parse error. missing ; before statement
+Line 564: Parse error. syntax error
+Line 565: Parse error. syntax error
+Line 572: Parse error. syntax error
+Line 573: Parse error. missing ; before statement
+Line 580: Parse error. syntax error
+Line 582: Parse error. syntax error
+Line 583: Parse error. syntax error
+Line 585: Parse error. syntax error
+Line 586: Parse error. syntax error
+Line 587: Parse error. syntax error
+Line 590: Parse error. syntax error
+Line 591: Parse error. syntax error
+Line 592: Parse error. missing ; before statement
+Line 593: Parse error. syntax error
+Line 594: Parse error. syntax error
+Line 595: Parse error. missing ; before statement
+Line 596: Parse error. syntax error
+Line 597: Parse error. syntax error
+Line 598: Parse error. syntax error
+Line 599: Parse error. syntax error
+Line 600: Parse error. syntax error
+Line 601: Parse error. missing ; before statement
+Line 602: Parse error. syntax error
+Line 604: Parse error. syntax error
+Line 606: Parse error. syntax error
+Line 607: Parse error. syntax error
+Line 615: Parse error. syntax error
+Line 616: Parse error. syntax error
+Line 617: Parse error. syntax error
+Line 623: Parse error. syntax error
+Line 625: Parse error. TypeError: redeclaration of const c.
+Line 627: Parse error. syntax error
+Line 628: Parse error. syntax error
+Line 630: Parse error. missing ; before statement
+Line 633: Parse error. syntax error
+Line 635: Parse error. syntax error
+Line 636: Parse error. syntax error
+Line 637: Parse error. missing ; before statement
+Line 638: Parse error. syntax error
+Line 639: Parse error. syntax error
+Line 640: Parse error. syntax error
+Line 641: Parse error. syntax error
+Line 642: Parse error. missing ; before statement
+Line 646: Parse error. syntax error
+Line 648: Parse error. missing ; before statement
+Line 649: Parse error. missing ; before statement
+Line 650: Parse error. syntax error
+Line 651: Parse error. syntax error
+Line 652: Parse error. syntax error
+Line 653: Parse error. syntax error
+Line 654: Parse error. missing ; before statement
+Line 658: Parse error. syntax error
+Line 660: Parse error. missing ; before statement
+Line 661: Parse error. missing ; before statement
+Line 662: Parse error. syntax error
+Line 663: Parse error. syntax error
+Line 664: Parse error. syntax error
+Line 665: Parse error. syntax error
+Line 666: Parse error. missing ; before statement
+Line 670: Parse error. syntax error
+Line 672: Parse error. missing ; before statement
+Line 674: Parse error. syntax error
+Line 675: Parse error. missing ; before statement
+Line 676: Parse error. missing ; before statement
+Line 677: Parse error. syntax error
+Line 681: Parse error. TypeError: redeclaration of const c.
+Line 683: Parse error. invalid return
+Line 692: Parse error. syntax error
+Line 694: Parse error. missing ; before statement
+Line 695: Parse error. missing ; before statement
+Line 696: Parse error. syntax error
+Line 697: Parse error. syntax error
+Line 698: Parse error. missing ; before statement
+Line 699: Parse error. missing ; before statement
+Line 700: Parse error. syntax error
+Line 701: Parse error. syntax error
+Line 702: Parse error. syntax error
+Line 707: Parse error. syntax error
+Line 709: Parse error. missing ; before statement
+Line 710: Parse error. syntax error
+Line 711: Parse error. syntax error
+Line 712: Parse error. syntax error
+Line 713: Parse error. syntax error
+Line 714: Parse error. syntax error
+Line 715: Parse error. syntax error
+Line 717: Parse error. syntax error
+Line 718: Parse error. syntax error
+Line 721: Parse error. invalid return
+Line 723: Parse error. invalid return
+Line 725: Parse error. syntax error
+Line 726: Parse error. syntax error
+Line 728: Parse error. syntax error
+Line 729: Parse error. syntax error
+Line 731: Parse error. syntax error
+Line 746: Parse error. missing ; before statement
+Line 789: Parse error. missing ; before statement
+Line 790: Parse error. missing ; before statement
+Line 792: Parse error. syntax error
+Line 793: Parse error. missing ; before statement
+Line 794: Parse error. syntax error
+Line 803: Parse error. syntax error
+Line 804: Parse error. missing ; before statement
+Line 806: Parse error. syntax error
+Line 807: Parse error. missing ; before statement
+Line 808: Parse error. syntax error
+Line 810: Parse error. syntax error
+Line 811: Parse error. missing ; before statement
+Line 813: Parse error. syntax error
+Line 814: Parse error. missing ; before statement
+Line 815: Parse error. syntax error
+Line 820: Parse error. syntax error
+Line 821: Parse error. missing ; before statement
+Line 823: Parse error. syntax error
+Line 824: Parse error. missing ; before statement
+Line 825: Parse error. syntax error
+Line 829: Parse error. syntax error
+Line 830: Parse error. missing ; before statement
+Line 842: Parse error. syntax error
+Line 843: Parse error. missing ; before statement
+Line 845: Parse error. TypeError: redeclaration of const c.
+Line 846: Parse error. TypeError: redeclaration of const d.
+Line 847: Parse error. TypeError: redeclaration of const e.
+Line 864: Parse error. syntax error
+Line 865: Parse error. missing ; before statement
+Line 868: Parse error. TypeError: redeclaration of const c.
+Line 876: Parse error. syntax error
+Line 877: Parse error. missing ; before statement
+Line 879: Parse error. syntax error
+Line 880: Parse error. missing ; before statement
+Line 881: Parse error. syntax error
+Line 885: Parse error. syntax error
+Line 886: Parse error. missing ; before statement
+Line 898: Parse error. syntax error
+Line 899: Parse error. missing ; before statement
+Line 901: Parse error. TypeError: redeclaration of const c.
+Line 902: Parse error. TypeError: redeclaration of const d.
+Line 903: Parse error. TypeError: redeclaration of const e.
+Line 920: Parse error. syntax error
+Line 921: Parse error. missing ; before statement
+Line 924: Parse error. TypeError: redeclaration of const c.
+Line 932: Parse error. syntax error
+Line 933: Parse error. missing ; before statement
+Line 935: Parse error. syntax error
+Line 936: Parse error. missing ; before statement
+Line 937: Parse error. syntax error
+Line 941: Parse error. syntax error
+Line 942: Parse error. missing ; before statement
+Line 943: Parse error. syntax error
+Line 950: Parse error. syntax error
+Line 951: Parse error. syntax error
+Line 953: Parse error. TypeError: redeclaration of const b.
+Line 954: Parse error. TypeError: redeclaration of const c.
+Line 955: Parse error. TypeError: redeclaration of const d.
+Line 956: Parse error. TypeError: redeclaration of const e.
+Line 980: Parse error. syntax error
+Line 981: Parse error. syntax error
+Line 984: Parse error. TypeError: redeclaration of const b.
+Line 994: Parse error. syntax error
+Line 995: Parse error. syntax error
+Line 996: Parse error. syntax error
+Line 1010: Parse error. syntax error
+Line 1010: Parse error. illegal character
+Line 1010: Parse error. missing ; before statement
+Line 1010: Parse error. illegal character
+Line 1022: Parse error. syntax error
+Line 1025: Parse error. syntax error
+Line 1026: Parse error. syntax error
+Line 1027: Parse error. missing ; before statement
+Line 1028: Parse error. syntax error
+Line 1029: Parse error. syntax error
+Line 1031: Parse error. TypeError: redeclaration of const b.
+Line 1046: Parse error. syntax error
+Line 1047: Parse error. missing ; before statement
+Line 1053: Parse error. syntax error
+Line 1054: Parse error. missing ; before statement
+Line 1067: Parse error. syntax error
+Line 1068: Parse error. syntax error
+Line 1069: Parse error. syntax error
+Line 1070: Parse error. missing ; before statement
+Line 1075: Parse error. syntax error
+Line 1076: Parse error. syntax error
+Line 1085: Parse error. syntax error
+Line 1086: Parse error. syntax error
+Line 1088: Parse error. TypeError: redeclaration of const b.
+Line 1099: Parse error. syntax error
+Line 1100: Parse error. syntax error
+Line 1101: Parse error. syntax error
+Line 1102: Parse error. syntax error
+Line 1103: Parse error. syntax error
+Line 1104: Parse error. missing ; before statement
+Line 1105: Parse error. syntax error
+Line 1106: Parse error. syntax error
+Line 1107: Parse error. missing ; before statement
+Line 1108: Parse error. syntax error
+Line 1109: Parse error. syntax error
+Line 1110: Parse error. syntax error
+Line 1112: Parse error. syntax error
+Line 1113: Parse error. syntax error
+Line 1115: Parse error. missing ; before statement
+Line 1117: Parse error. syntax error
+Line 1118: Parse error. missing ; before statement
+Line 1119: Parse error. missing ; before statement
+Line 1121: Parse error. syntax error
+Line 1123: Parse error. syntax error
+Line 1125: Parse error. missing ; before statement
+Line 1127: Parse error. missing ; before statement
+Line 1128: Parse error. invalid return
+Line 1129: Parse error. syntax error
+Line 1130: Parse error. syntax error
+Line 1131: Parse error. syntax error
+Line 1133: Parse error. syntax error
+Line 1134: Parse error. missing ; before statement
+Line 1140: Parse error. syntax error
+Line 1141: Parse error. missing ; before statement
+Line 1142: Parse error. syntax error
+Line 1143: Parse error. invalid return
+Line 1143: Parse error. illegal character
+
